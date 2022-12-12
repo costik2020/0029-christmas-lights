607f0560e5283dd9eda0706a7b850d322f76af66
costikluciu2020 <costikluciu2020@gmail.com>
2022-12-12 12:33:00 +0000

    Added 8 flash options for my christmas lights

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ let christmasButton = document.querySelector(".christmas-button");
 let christmasWiresFrame = document.querySelector("#christmas-wires-frame");
 
 console.log(christmasButton);
-let flashArray = ["frame-flash1", "frame-flash2", "frame-flash3","frame-flashNONE", "frame-flash4"];
+//let flashArray = [ "frame-flash1", "frame-flash2", "frame-flash3", "frame-flash4", "frame-flash5", "frame-flash6", "frame-flash7", "frame-flash8"];
+
+let flashArray = [ "frame-flash1", "frame-flash2", "frame-flash3", "frame-flash6", "frame-flash4", "frame-flash5", "frame-flash7", "frame-flash8"];
+
 
 let flashClicked = 0;
 christmasButton.addEventListener("click", (e)=>{
-    console.log("clicked!");
     flashClicked = flashClicked + 1;
     let index = flashClicked % flashArray.length;
-    console.log("index=", index);
+    console.log("flashOption=", flashArray[index]);
     christmasWiresFrame.className = flashArray[index]; 
 })
 
